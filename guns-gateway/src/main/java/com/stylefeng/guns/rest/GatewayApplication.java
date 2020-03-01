09b05b8a978141df9e09b625faf08e7065571554
wangwei <wang_wei608@163.com>
2020-03-01 15:25:00 +0800

    熔断和限流的使用

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/GatewayApplication.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/GatewayApplication.java
@@ -3,11 +3,15 @@ package com.stylefeng.guns.rest;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
 @EnableDubboConfiguration
 @EnableAsync
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class GatewayApplication {
 
     public static void main(String[] args) {
--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/order/OrderController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/order/OrderController.java
@@ -4,6 +4,8 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.david.meeting.api.order.OrderServiceApi;
 import com.david.meeting.api.order.vo.OrderVO;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.stylefeng.guns.core.util.TokenBucket;
 import com.stylefeng.guns.rest.common.CurrentUser;
 import com.stylefeng.guns.rest.common.vo.ResponseVO;
@@ -28,6 +30,53 @@ public class OrderController {
     @Reference(interfaceClass = OrderServiceApi.class, check = false)
     private OrderServiceApi orderServiceApi;
 
+    /*
+        hystrix隔离:
+        信号量隔离
+        线程池隔离
+        线程切换
+     */
+    @HystrixCommand(fallbackMethod = "error", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.strategy", value = "THREAD"),
+
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "4000"),
+            // 滑动统计的桶数量
+            /**
+             * 设置一个rolling window被划分的数量，若numBuckets＝10，rolling window＝10000，
+             *那么一个bucket的时间即1秒。必须符合rolling window % numberBuckets == 0。默认1
+             */
+//            @HystrixProperty(name = "metrics.rollingStats.numBuckets", value = "10"),
+            // 设置滑动窗口的统计时间。熔断器使用这个时间
+            /** 设置统计的时间窗口值的，毫秒值。
+             circuit break 的打开会根据1个rolling window的统计来计算。
+             若rolling window被设为10000毫秒，则rolling window会被分成n个buckets，
+             每个bucket包含success，failure，timeout，rejection的次数的统计信息。默认10000
+             **/
+//            @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000")},
+            /**
+             * 设置在一个滚动窗口中，打开断路器的最少请求数。
+             比如：如果值是20，在一个窗口内（比如10秒），收到19个请求，即使这19个请求都失败了，断路器也不会打开。
+             */
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+            //当出错率超过50%后熔断器启动
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50")},
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "15"),
+                    /**
+                     * BlockingQueue的最大队列数，当设为-1，会使用SynchronousQueue，值为正时使用LinkedBlcokingQueue。
+                     */
+                    @HystrixProperty(name = "maxQueueSize", value = "15"),
+                    /**
+                     * 设置存活时间，单位分钟。如果coreSize小于maximumSize，那么该属性控制一个线程从实用完成到被释放的时间.
+                     */
+                    @HystrixProperty(name = "keepAliveTimeMinutes", value = "2"),
+                    /**
+                     * 设置队列拒绝的阈值,即使maxQueueSize还没有达到
+                     */
+                    @HystrixProperty(name = "queueSizeRejectionThreshold", value = "15"),
+                    @HystrixProperty(name = "metrics.rollingStats.numBuckets", value = "10"),
+                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "10000")
+            })
     // 购票
     @PostMapping("buyTickets")
     public ResponseVO buyTickets(@RequestParam Integer fieldId, String soldSeats, String seatsName) {
@@ -64,6 +113,11 @@ public class OrderController {
             return ResponseVO.serviceFail("购票业务失败");
         }
     }
+
+    public ResponseVO error(@RequestParam Integer fieldId, String soldSeats, String seatsName) {
+        return ResponseVO.serviceFail("抱歉，下单人数太多，请稍后重试");
+    }
+
 
 
     @GetMapping("getOrderInfo")
